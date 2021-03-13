0d4f1734d0d67a846eba1fd10083a4c1a1f83f48
Mikael M <mikael_96@live.com>
2021-03-13 18:56:00 +0200

    mapping plantsWithGrowZoneFlow to provide suspend function that is main-safe

--- advanced-coroutines-codelab/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ advanced-coroutines-codelab/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -88,6 +88,11 @@ class PlantRepository private constructor(
      */
     fun getPlantsWithGrowZoneFlow(growZone: GrowZone): Flow<List<Plant>> {
         return plantDao.getPlantsWithGrowZoneNumberFlow(growZone.number)
+            .map { plantList ->
+                val sortOrderFromNetwork = plantsListSortOrderCache.getOrAwait()
+                val nextValue = plantList.applyMainSafeSort(sortOrderFromNetwork)
+                nextValue
+            }
     }
 
     /**
